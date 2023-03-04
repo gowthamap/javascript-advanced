3b0a45976d6c6a0926fe7e15a42bfe6c37633252
Gowthamap <gowthamap55555@gmail.com>
2023-03-05 01:24:50 +0530

    feat: destructuring

--- index.js
+++ index.js
@@ -1,40 +1,16 @@
-var objA = {
-  a: 1,
-  b: 2,
-  c: "Hello",
+const obj1 = {
+  name: "Code Malayalam",
+  place: "kerala",
+};
+const obj2 = {
+  name: "Bob",
+  place: "America",
 };
 
-var objB = {
-  a1: 1,
-  b1: 2,
-  c1: "Hello 2 ",
-};
+function abc() {
+  var { name, place } = obj1;
+  var { name: name2, place: place2 } = obj2;
+  console.log(name, place, name2, place2);
+}
 
-var obj2 = { ...objA, ...objB, z:32 };
-
-console.log(obj2);
-
-var a = [1, 2, 3, 4];
-var b = [6,7,8,9]
-var c = [...a, ...b]
-
-console.log(c);
-
-var obj1 = {
-  a: 1,
-  b: 2,
-  c : {
-    name: 'raman',
-    age: 45
-  }
-};
-
-var obj2 = {...obj1}
-
-obj1.d = 44
-obj1.c.name = 'krishnan'
-console.log(obj1 , obj2);
-
-var arr = [1, 2, 33, 49, 15, 6];
-var a = Math.max(...arr);
-console.log(a);
+abc();
