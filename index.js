2bcaf24ccf3b6e62c3b5a8ba1b2bbf9680fd6274
Gowthamap <gowthamap55555@gmail.com>
2023-03-04 22:25:51 +0530

    feat: Closure

--- index.js
+++ index.js
@@ -1,16 +1,55 @@
-function one() {
-  var a = {
-    value: 100,
+// function add(a, b) {
+//   function myAdd() {
+//     const c = a + b;
+//     return c;
+//   }
+//   return myAdd;
+// }
+
+// function main() {
+//   const res = add(3, 4);
+//   const val = res();
+//   console.dir(res);
+// }
+
+// main();
+
+// function main(a) {
+//   [1, 2, 3, 4, 5].forEach(function (i) {
+//     console.log(i, a);
+//   });
+// }
+
+// main(10);
+
+var objs = [
+  {
+    name: "One",
+    msg: "Hello I am one",
+  },
+  {
+    name: "Two",
+    msg: "Hello I am Two",
+  },
+  {
+    name: "Three",
+    msg: "Hello I am Three",
+  },
+];
+
+function main() {
+  objs.forEach((item) => {
+    const bt = document.createElement("button");
+    bt.innerHTML = item.name;
+    bt.onclick = getCallBack(item.msg);
+    document.body.appendChild(bt);
+  });
+
+  function getCallBack(arg) {
+    return function () {
+      alert(arg);
+    };
   }
-  two(a);
-  console.log("one", a.value);
 }
 
-function two(a) {
-  a = {
-    value: 400
-  };
-  console.log("two", a.value);
-}
-
-one();
+main();
