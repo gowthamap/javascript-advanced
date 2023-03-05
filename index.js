18882ab57d68af07166e4bf85232b4cee57194f2
Gowthamap <gowthamap55555@gmail.com>
2023-03-05 16:50:04 +0530

    feat: class in javascript

--- index.js
+++ index.js
@@ -1,30 +1,35 @@
-var obj = {
-  name: "Alice",
-  mark: 35,
-  place: "Kannur",
-  subject: {
-    teacher: "Bob",
-  },
-};
+class Student {
+  name;
+  yob;
+  place;
+  constructor(name, yob) {
+    this.name = name;
+    this.yob = yob;
+  }
 
-var {
-  subject: { teacher },
-} = obj;
+  getAge() {
+    return new Date().getFullYear() - this.yob;
+  }
+  getName() {
+    return this.name;
+  }
+}
 
-console.log(teacher);
+Student.printMinYob = function(...args) {
+  var arr = args.map((item) => item.yob);
+  console.log(Math.min(...arr));
+}
 
-var arr = [
-  {
-    name: "Alice",
-    mark: 35,
-    place: "Kannur",
-    subject: {
-      teacher: "Bob",
-    },
-  },
-  22,
-  33,
-];
-var [{subject: {teacher}}] = arr;
+var student1 = new Student("Alen", 1990);
 
-console.log(teacher);
+var student2 = new Student("Bob", 1995);
+
+var student3 = new Student("Cook", 1980);
+
+console.log(student1,student1.getName(), student1.getAge());
+console.log(student2,student2.getName(), student2.getAge());
+console.log(student3,student3.getName(), student3.getAge());
+
+Student.printMinYob(student1, student2, student3)
+
+ 
